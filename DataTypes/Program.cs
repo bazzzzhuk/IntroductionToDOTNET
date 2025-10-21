a80d5234dc51fb23baa8cc5b63d2dbd043dfd421
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2025-10-22 00:44:08 +0300

    We must save time...

---
+++ DataTypes/Program.cs
@@ -0,0 +1,64 @@
+﻿//#define DATA_TYPES
+//#define DATA_TYPES
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+	internal class Program
+	{
+		const string delimiter = "\n=================================\n";
+		static void Main(string[] args)
+		{
+#if DATA_TYPES
+			Console.WriteLine($"bool занимает {sizeof(bool)} Байт памяти, класс-обёртка Boolean");
+			Console.WriteLine(bool.FalseString);
+			Console.WriteLine(bool.TrueString);
+			Console.WriteLine(typeof(bool));
+
+			////////////////////////////////////////////////////////////////////
+			// @ - RAW-строка игнорирует все специальные символы и ESC-последовательности, т.е. воспринимается как есть.
+			Console.WriteLine(delimiter);
+			Console.WriteLine
+				(
+$@"Тип данных char занимает {sizeof(char)} Байт памяти 
+и принимает значение в диапазоне от {(int)char.MinValue} до {(int)char.MaxValue}. Класс-обёртка - {typeof(char)}");
+			Console.WriteLine(delimiter);
+			Console.WriteLine
+				(
+$@"@-RAW - строка игнорирует все специальные символы и ESC - последовательности,
+т.е.воспринимается как есть."
+				);
+			Console.WriteLine(delimiter);
+			Console.WriteLine
+				(
+$@"byte занимает {sizeof(byte)} Байт памяти,
+и принимает значения в диапазоне от {byte.MinValue} до {byte.MaxValue},
+Класс-обёртка - {typeof(byte)} "
+				);
+			Console.WriteLine(delimiter);
+			Console.WriteLine
+				(
+$@"sbyte занимает {sizeof(sbyte)} Байт памяти,
+и принимает значения в диапазоне от {sbyte.MinValue} до {sbyte.MaxValue},
+Класс-обёртка - {typeof(sbyte)} "
+				);
+			Console.WriteLine(delimiter);
+			Console.WriteLine
+				(
+$@"sbyte занимает {sizeof(sbyte)} Байт памяти.");
+			Console.WriteLine(delimiter);
+			Console.WriteLine
+				(
+$@"sbyte занимает {sizeof(decimal)} Байт памяти.");
+			Console.WriteLine(delimiter);
+#endif
+
+
+
+		}
+	}
+}
---
+++ GameOver_1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOver_1
+{
+	class Rocket
+	{
+		int pos_x;
+		int pos_y;
+		void print_rocket(int pos_x, int pos_y)
+		{
+			Console.CursorLeft = pos_x + 1;
+			Console.CursorTop = pos_y;
+			Console.Write("▲");
+			Console.CursorLeft = pos_x + 1;
+			Console.CursorTop = pos_y + 1;
+			Console.Write("█");
+			Console.CursorLeft = pos_x;
+			Console.CursorTop = pos_y + 2;
+			Console.Write("▄█▄");
+			//Console.CursorLeft = pos_x;
+			//Console.CursorTop = pos_y + 3;
+			//Console.Write("▌▓▐");
+		}
+		void print_flame(int pos_x, int pos_y)
+		{
+			Console.CursorLeft = pos_x;
+			Console.CursorTop = pos_y + 3;
+			Console.Write("▌▓▐");
+			Console.CursorLeft = pos_x;
+			Console.CursorTop = pos_y + 3;
+			Console.Write("▌░▐");
+		}
+		Rocket(int x, int y)
+		{
+			this.pos_x = x;
+			this.pos_y = y;
+		}
+		void Rocket_move(ConsoleKey key2)
+		{
+			Random rand = new Random();
+			Console.WriteLine(rand.Next(10)+rand.Next(10));
+
+			if (key2 == ConsoleKey.LeftArrow) this.pos_x--;
+			if (key2 == ConsoleKey.RightArrow) this.pos_x++;
+			if (key2 == ConsoleKey.UpArrow) this.pos_y--;
+			if (key2 == ConsoleKey.DownArrow) this.pos_y++;
+			this.print_rocket(pos_x, pos_y);
+			this.print_flame(pos_x, pos_y);
+		}
+
+		internal class Program
+		{
+			static void Main(string[] args)
+			{
+				int x = Console.CursorLeft = 25;
+				int y = Console.CursorTop = 25;
+				Rocket rocket = new Rocket(x, y);
+				Console.CursorVisible = false;
+				//do
+				//{
+				//}
+				//while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape)) ;
+				bool isQuit = false;
+				while (!isQuit)
+				{
+					var task = Task.Factory.StartNew(() => { return Console.ReadKey(true); });
+					if (task.Wait(-1)) // wait forever
+					{
+						//Console.Write(task.Result.Key.ToString());
+						Console.Clear();
+						rocket.Rocket_move(task.Result.Key);
+						if (task.Result.Key == ConsoleKey.Q)
+						{
+							//ConsoleKey key2 = Console.CancelKeyPress();
+							isQuit = true;
+						}
+					}
+				}
+			}
+
+		}
+	}
+}
