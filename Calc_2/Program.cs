4ff916f636785b07cba146a68be27caa18b9eba7
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2025-10-28 16:41:37 +0300

    in progress check

--- Calc_2/Program.cs
+++ Calc_2/Program.cs
@@ -20,19 +20,21 @@ namespace Calc_2
 		static string str, str1, str2, str_in;
 		static void Main(string[] args)
 		{
-			str=Enter_expression();
-			//Console.WriteLine(str);
-			Calc_2(str);
+			double rezult = Convert.ToDouble(Calc_2(Enter_expression()));
+			Console.BackgroundColor = ConsoleColor.White;
+			Console.ForegroundColor = ConsoleColor.Black;
+			Console.WriteLine($"{rezult}  <--- Результат выражения");
+			Console.ResetColor();
+
+
 		}
 		static string Enter_expression()
 		{
 			//Console.Write("Введите арифметическое выражение:");
 			//str = Console.ReadLine();
 			//str="12345 + (5 * 2) / 2 - (12 / 3) + 678";
-			str = "(12+5)*2/4-12/(3+67)+*123";
-			//bool ooo = security_str(str);
-			//Console.WriteLine(ooo);
-			if (security_str(str)) 
+			str = "1+((12*5)*2/4-(12/3*2+6)+(123*2/246)";
+			if (security_str(str)&&check_bracket(str))				
 			{ 
 				return str;
 			}
@@ -45,30 +47,20 @@ namespace Calc_2
 		{
 			for (int i = 0;	i<str.Length-1;i++)
 			{
-				//Console.WriteLine(str);
-				//Console.WriteLine();
-				//Console.Write(str[i]);
-				//Console.Write(" ");
-				//Console.WriteLine(Convert.ToString(str[i]).IndexOfAny(chars));
-				if (chars.Contains(str[i]) && chars.Contains(str[i + 1]))
-				{
-					//Console.WriteLine($"> {str[i]} <~~~> {str[i+1]} <");
-					return false;
-				}/*else Console.WriteLine("not")*/;
+				if (chars.Contains(str[i]) && chars.Contains(str[i + 1]))return false;
 			}
 			return true;
 		}
-		static void Calc_2(string str)
+		static string Calc_2(string str)
 		{
 			str = str.Replace(" ", string.Empty);
 			Console.WriteLine(str);
 			str = Bracket(str);
 			str = Priority(str, priority);
 			str = Priority(str, chars);
-			Console.BackgroundColor = ConsoleColor.White;
-			Console.ForegroundColor = ConsoleColor.Black;
-			Console.WriteLine($"{str} <--- Результат выражения");
-			Console.ResetColor();
+			
+			
+			return str;
 		}
 		static string Calc(string str_in)
 		{
@@ -95,19 +87,52 @@ namespace Calc_2
 		{
 			do//проверка скобок и их упрощение
 			{
+				
 				int bracket1;
 				int bracket2;
+				int bracket3;
 				bracket1 = str.IndexOf('(');
 				bracket2 = str.IndexOf(')');
+				bracket3 = str.IndexOf('(',bracket1+1);
+				if (bracket3 < bracket2) Console.WriteLine("dsgdfbdfbdf");
+
+				Console.WriteLine($"bracket1 {bracket1}");
 				if (bracket1 == bracket2) break;
 			Console.WriteLine("Приоритет скобок:");
 				str_in = str.Substring(bracket1 + 1, bracket2 - bracket1 - 1);
 				str = str.Remove(bracket1, bracket2 - bracket1 + 1);
-				str = str.Insert(bracket1, Calc(str_in));
+				str = str.Insert(bracket1, Calc_2(str_in));
 				Console.WriteLine(str);
 			} while (true);
 			return str;
 		}
+		static bool check_bracket(string str)
+		{
+			int bracket1=0;
+			int bracket2=0;
+			do
+			{
+				//Console.ReadKey();	
+				Console.WriteLine(bracket1);
+				Console.WriteLine(bracket2);
+				if(bracket1==-1||bracket2==-1)return false;
+				else if(bracket1 == bracket2)
+				{
+			
+					Console.WriteLine("Dis");
+					return true;
+				}
+				bracket1 = str.IndexOf('(', bracket1 + 1);
+				bracket2 = str.IndexOf(')', bracket2 + 1);
+				bracket1++;
+				bracket2++;
+			//bracket1 = str.IndexOf('(',bracket1);
+			//bracket2 = str.IndexOf(')',bracket2);
+			} while (bracket1 != -1||bracket2!=-1);
+			return true;
+		}
+
+
 		static string Priority(string str, char[] op)
 		{
 			do//Решения по приоритету 
