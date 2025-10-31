dcc34a39fab6989b21db9f6ee8368257f39c20bc
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2025-10-31 18:30:13 +0300

    small edit

--- Calc_2/Program.cs
+++ Calc_2/Program.cs
@@ -1,5 +1,5 @@
 ﻿//#define STEP
-//#define BRACKET_CHECK_MAD
+#define BRACKET_CHECK_MAD
 
 using System;
 using System.Collections.Generic;
@@ -14,7 +14,7 @@ namespace Calc_2
 	internal class Program
 	{
 		static int oper;
-		static char[] chars = { '-', '+', '*', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '(', ')', '.', ',' };
+		static char[] chars = { '-', '+', '*', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '(', ')', ',' };
 		static char[] operators = { '-', '+', '*', '/' };
 		static char[] priority = { '*', '/' };
 		static string str, str1, str2, str_in;
@@ -35,7 +35,7 @@ namespace Calc_2
 			//Console.Write("Введите арифметическое выражение:");
 			//str = Console.ReadLine();
 			//str="12345 + (5 * 2) / 2 - (12 / 3) + 678";
-			str = "1+(12*5)*2/4-(((12/0+1)*2+6)*1)+(12.3*(2/246))";
+			str = "1+3*(12*5)*20/4-(((12/2+1)+(2*6))*10)+(12.3+(2/246))";
 			str = str.Replace(" ", string.Empty);
 			str = str.Replace(".", ",");
 			Console.BackgroundColor = ConsoleColor.DarkYellow;
@@ -43,8 +43,9 @@ namespace Calc_2
 			Console.WriteLine("↓ ↓ ↓ ВЫРАЖЕНИЕ ↓ ↓ ↓");
 			Console.BackgroundColor = ConsoleColor.White;
 			Console.ForegroundColor = ConsoleColor.Black;
-			Console.WriteLine(str);
+			Console.WriteLine(str); 
 			Console.ResetColor();
+			bracket_check_mad(str);
 			if (security_str(str) && check_bracket(str)) return str;
 			else return "0";
 		}
@@ -56,7 +57,7 @@ namespace Calc_2
 				if (operators.Contains(str[str.Length - 1])) return Print_cerr("оператор в конце выражения", str.Length - 1);
 				if (operators.Contains(str[0])) return Print_cerr("оператор в начале выражения", 0);
 				if (operators.Contains(str[i]) && operators.Contains(str[i + 1])) return Print_cerr("два оператора подряд", i);
-				if (str[i] == ')' && str[i+1]=='(')return Print_cerr("неправильная последовательность скобок",i);
+				if ((str[i] == ')' && str[i + 1] == '(') || (str[i] == '(' && str[i + 1] == ')')) return Print_cerr("неправильная последовательность скобок", i);
 			}
 			return true;
 		}
@@ -132,17 +133,26 @@ namespace Calc_2
 					Console.ResetColor();
 					return true;
 				}
+				if (bracket2 < bracket1) return Print_cerr("неправильное расположение скобок",bracket2);
 				bracket1++;
 				bracket2++;
 			} while (bracket1 != bracket2);
-#if BRACKET_CHECK_MAD
-			do//проверка на безобразие скобок: ")("(решено в security), "a+b)+(c-d"
-			{
-
-			} while (bracket1 != bracket2);
-#endif
 			return true;
 		}
+#if BRACKET_CHECK_MAD
+		static bool bracket_check_mad(string str)
+		{
+			char[] bracket = { '(', ')' };
+			int bracket1 = str.IndexOfAny(bracket);
+			int bracket2 = 0;
+			//Console.WriteLine(bracket1);
+			//do//проверка на безобразие скобок: ")("(решено в security), "a+b)+(c-d"
+			//{
+
+			//} while (true);
+			return true;	
+		}
+#endif
 		static string Priority(string str, char[] op)
 		{
 			do//Решения по приоритету 
